cd08ddaa720a6a3d2c66918583f16148fac1a4a6
abin duwal <aintmea7@gmail.com>
2022-08-22 15:40:38 +0545

    new files

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,14 +1,12 @@
-const { readFileSync, writeFileSync } = require("fs");
-console.log('start');
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
-
-console.log(first, second);
+const { readFileSync, writeFileSync } = require('fs')
+console.log('start')
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
 
 writeFileSync(
-  "./content/result-sync.txt",
-  `Here is the result: ${first},${second}`,
-  { flag: "a" }
-);
-console.log('done with the task');
-console.log('starting the next');
+  './content/result-sync.txt',
+  `Here is the result : ${first}, ${second}`,
+  { flag: 'a' }
+)
+console.log('done with this task')
+console.log('starting the next one')
